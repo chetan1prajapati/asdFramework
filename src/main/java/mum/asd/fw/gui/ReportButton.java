3b686704cd45e3626ca34c02ec115a1358e3fe1d
Chetan <Chetan@172.17.0.124>
2015-04-22 03:35:02 -0500

    ReportButton

---
+++ src/main/java/mum/asd/fw/gui/ReportButton.java
@@ -0,0 +1,19 @@
+package mum.asd.fw.gui;
+
+
+public class ReportButton {
+	private String label;
+
+	public ReportButton(String label) {
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+}
